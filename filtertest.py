865c8cd7c880f4e8aea4ed3e57c06cdbfa5e7e7f
cuongvu997 <cuongqv97@gmail.com>
2020-04-01 17:16:14 +0700

    1/4

--- filtertest.py
+++ filtertest.py
@@ -3,7 +3,7 @@ import cv2 # For OpenCV modules (For Image I/O and Contour Finding)
 import numpy as np # For general purpose array manipulation
 import scipy.fftpack # For FFT2 
 from lib_detection import load_model, detect_lp, im2single
-
+import PIL
 
 # Ham sap xep contour tu trai sang phai
 def sort_contours(cnts):
@@ -29,42 +29,44 @@ def fine_tune(lp):
 
 # imclearborder definition
 
-def imclearborder(imgBW, radius):
-
-    # Given a black and white image, first find all of its contours
-    imgBWcopy = imgBW.copy()
-    contours,hierarchy = cv2.findContours(imgBWcopy.copy(), cv2.RETR_LIST, 
-        cv2.CHAIN_APPROX_SIMPLE)
-
-    # Get dimensions of image
-    imgRows = imgBW.shape[0]
-    imgCols = imgBW.shape[1]    
-
-    contourList = [] # ID list of contours that touch the border
-
-    # For each contour...
-    for idx in np.arange(len(contours)):
-        # Get the i'th contour
-        cnt = contours[idx]
-
-        # Look at each point in the contour
-        for pt in cnt:
-            rowCnt = pt[0][1]
-            colCnt = pt[0][0]
-
-            # If this is within the radius of the border
-            # this contour goes bye bye!
-            check1 = (rowCnt >= 0 and rowCnt < radius) or (rowCnt >= imgRows-1-radius and rowCnt < imgRows)
-            check2 = (colCnt >= 0 and colCnt < radius) or (colCnt >= imgCols-1-radius and colCnt < imgCols)
-
-            if check1 or check2:
-                contourList.append(idx)
-                break
-
-    for idx in contourList:
-        cv2.drawContours(imgBWcopy, contours, idx, (0,0,0), -1)
-
-    return imgBWcopy
+# =============================================================================
+# def imclearborder(imgBW, radius):
+# 
+#     # Given a black and white image, first find all of its contours
+#     imgBWcopy = imgBW.copy()
+#     contours,hierarchy = cv2.findContours(imgBWcopy.copy(), cv2.RETR_LIST, 
+#         cv2.CHAIN_APPROX_SIMPLE)
+# 
+#     # Get dimensions of image
+#     imgRows = imgBW.shape[0]
+#     imgCols = imgBW.shape[1]    
+# 
+#     contourList = [] # ID list of contours that touch the border
+# 
+#     # For each contour...
+#     for idx in np.arange(len(contours)):
+#         # Get the i'th contour
+#         cnt = contours[idx]
+# 
+#         # Look at each point in the contour
+#         for pt in cnt:
+#             rowCnt = pt[0][1]
+#             colCnt = pt[0][0]
+# 
+#             # If this is within the radius of the border
+#             # this contour goes bye bye!
+#             check1 = (rowCnt >= 0 and rowCnt < radius) or (rowCnt >= imgRows-1-radius and rowCnt < imgRows)
+#             check2 = (colCnt >= 0 and colCnt < radius) or (colCnt >= imgCols-1-radius and colCnt < imgCols)
+# 
+#             if check1 or check2:
+#                 contourList.append(idx)
+#                 break
+# 
+#     for idx in contourList:
+#         cv2.drawContours(imgBWcopy, contours, idx, (0,0,0), -1)
+# 
+#     return imgBWcopy
+# =============================================================================
 
 # bwareaopen definition
 def bwareaopen(imgBW, areaPixels):
@@ -120,15 +122,13 @@ print("Detection Execution time: " + str(end-start))
 if (len(LpImg)):
 
     # Chuyen doi anh bien so
-    LpImg[0] = cv2.convertScaleAbs(LpImg[0], alpha=(255.0))
-
-    roi = LpImg[0]
+    roi = cv2.convertScaleAbs(LpImg[0], alpha=(255.0))
     
     cv2.imshow("anh bien so detect",roi)
     cv2.waitKey()   
     
     
-    if 0.7< roi.shape[0]/roi.shape[1]<1.5:
+    if 0.7 < roi.shape[0]/roi.shape[1] < 1.5:
         image = roi
 
         cv2.imshow("Original Image", image)
@@ -158,91 +158,117 @@ if (len(LpImg)):
         print (start_row, end_row) 
         print (start_col, end_col)
         
+        img2 = cropped_bot
         cv2.imshow("Cropped Bot", cropped_bot) 
         cv2.waitKey()    
         
+        img1 = PIL.Image.fromarray(img1)
+        img2 = PIL.Image.fromarray(img2)
         
-    else:
+        img1.save("top.jpg")
+        img2.save("bottom.jpg")
         
-        # Chuyen anh bien so ve gray
-        gray = cv2.cvtColor( LpImg[0], cv2.COLOR_BGR2GRAY)
-       
-        # Main program
-        # Read in image
-        img = gray
+        list_im = ["top.jpg","bottom.jpg"]
+        imgs    = [ PIL.Image.open(i) for i in list_im ]
+        # pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
+        min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
+        imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
         
-        # Number of rows and columns
-        rows = img.shape[0]
-        cols = img.shape[1]
-        
-        
-        # Convert image to 0 to 1, then do log(1 + I)
-        imgLog = np.log1p(np.array(img, dtype="float") / 255)
-        
-        # Create Gaussian mask of sigma = 10
-        M = 2*rows + 1
-        N = 2*cols + 1
-        sigma = 10
-        (X,Y) = np.meshgrid(np.linspace(0,N-1,N), np.linspace(0,M-1,M))
-        centerX = np.ceil(N/2)
-        centerY = np.ceil(M/2)
-        gaussianNumerator = (X - centerX)**2 + (Y - centerY)**2
-        
-        # Low pass and high pass filters
-        Hlow = np.exp(-gaussianNumerator / (2*sigma*sigma))
-        Hhigh = 1 - Hlow
-        
-        # Move origin of filters so that it's at the top left corner to
-        # match with the input image
-        HlowShift = scipy.fftpack.ifftshift(Hlow.copy())
-        HhighShift = scipy.fftpack.ifftshift(Hhigh.copy())
-        
-        # Filter the image and crop
-        If = scipy.fftpack.fft2(imgLog.copy(), (M,N))
-        Ioutlow = np.real(scipy.fftpack.ifft2(If.copy() * HlowShift, (M,N)))
-        Iouthigh = np.real(scipy.fftpack.ifft2(If.copy() * HhighShift, (M,N)))
-        
-        # Set scaling factors and add
-        gamma1 = 0.3
-        gamma2 = 1.5
-        Iout = gamma1*Ioutlow[0:rows,0:cols] + gamma2*Iouthigh[0:rows,0:cols]
-        
-        # Anti-log then rescale to [0,1]
-        Ihmf = np.expm1(Iout)
-        Ihmf = (Ihmf - np.min(Ihmf)) / (np.max(Ihmf) - np.min(Ihmf))
-        Ihmf2 = np.array(255*Ihmf, dtype="uint8")
-        
-        # Threshold the image - Anything below intensity 65 gets set to white
-        Ithresh = Ihmf2 < 65
-        Ithresh = 255*Ithresh.astype("uint8")
-        
-        # Clear off the border.  Choose a border radius of 5 pixels
-        Iclear = imclearborder(Ithresh, 5)
-        
-        # Eliminate regions that have areas below 120 pixels
-        Iopen = bwareaopen(Iclear, 120)
-        
-        # Show all images
-        cv2.imshow('Original Image', img)
-        cv2.imshow('Homomorphic Filtered Result', Ihmf2)
-        cv2.imshow('Thresholded Result', Ithresh)
-        cv2.imshow('Opened Result', Iopen)
-        cv2.waitKey()
-        
+        # save that beautiful picture
+        imgs_comb = PIL.Image.fromarray(imgs_comb)
+        imgs_comb.save( 'square_sorted.jpg' )
+                
+        plateImg = cv2.imread('square_sorted.jpg')
+        roi = cv2.convertScaleAbs(plateImg, alpha=(255.0))
+        gray = cv2.cvtColor(plateImg, cv2.COLOR_BGR2GRAY)
       
-        # Segment kí tự
-        kernel3 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-        thre_mor = cv2.morphologyEx(Iopen, cv2.MORPH_DILATE, kernel3)
-        cont, _  = cv2.findContours(thre_mor, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    
-    
-        plate_info = ""
         
-        for c in sort_contours(cont):
+    else:    
+        # Chuyen anh bien so ve gray
+        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+     
+        cv2.waitKey()
+   
+    # Main program
+    # Read in image
+    img = gray
+    cv2.imshow("12d",img)
+    cv2.waitKey()
+    
+    # Number of rows and columns
+    rows = img.shape[0]
+    cols = img.shape[1]
+    
+    
+    # Convert image to 0 to 1, then do log(1 + I)
+    imgLog = np.log1p(np.array(img, dtype="float") / 255)
+    
+    # Create Gaussian mask of sigma = 10
+    M = 2*rows + 1
+    N = 2*cols + 1
+    sigma = 10
+    (X,Y) = np.meshgrid(np.linspace(0,N-1,N), np.linspace(0,M-1,M))
+    centerX = np.ceil(N/2)
+    centerY = np.ceil(M/2)
+    gaussianNumerator = (X - centerX)**2 + (Y - centerY)**2
+    
+    # Low pass and high pass filters
+    Hlow = np.exp(-gaussianNumerator / (2*sigma*sigma))
+    Hhigh = 1 - Hlow
+    
+    # Move origin of filters so that it's at the top left corner to
+    # match with the input image
+    HlowShift = scipy.fftpack.ifftshift(Hlow.copy())
+    HhighShift = scipy.fftpack.ifftshift(Hhigh.copy())
+    
+    # Filter the image and crop
+    If = scipy.fftpack.fft2(imgLog.copy(), (M,N))
+    Ioutlow = np.real(scipy.fftpack.ifft2(If.copy() * HlowShift, (M,N)))
+    Iouthigh = np.real(scipy.fftpack.ifft2(If.copy() * HhighShift, (M,N)))
+    
+    # Set scaling factors and add
+    gamma1 = 0.3
+    gamma2 = 1.5
+    Iout = gamma1*Ioutlow[0:rows,0:cols] + gamma2*Iouthigh[0:rows,0:cols]
+    
+    # Anti-log then rescale to [0,1]
+    Ihmf = np.expm1(Iout)
+    Ihmf = (Ihmf - np.min(Ihmf)) / (np.max(Ihmf) - np.min(Ihmf))
+    Ihmf2 = np.array(255*Ihmf, dtype="uint8")
+    
+    # Threshold the image - Anything below intensity 65 gets set to white
+    Ithresh = Ihmf2 < 65
+    Ithresh = 255*Ithresh.astype("uint8")
+    
+# =============================================================================
+#     # Clear off the border.  Choose a border radius of 5 pixels
+#     Iclear = imclearborder(Ithresh, 1)
+# =============================================================================
+    
+    # Eliminate regions that have areas below 120 pixels
+    Iopen = bwareaopen(Ithresh, 120)
+    
+    # Show all images
+    cv2.imshow('Original Image', img)
+    cv2.imshow('Homomorphic Filtered Result', Ihmf2)
+    cv2.imshow('Thresholded Result', Ithresh)
+    cv2.imshow('Opened Result', Iopen)
+    cv2.waitKey()
+    
+  
+    # Segment kí tự
+    kernel3 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    thre_mor = cv2.morphologyEx(Iopen, cv2.MORPH_DILATE, kernel3)
+    cont, _  = cv2.findContours(thre_mor, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+
+    plate_info = ""
+        
+    for c in sort_contours(cont):
             (x, y, w, h) = cv2.boundingRect(c)
             ratio = h/w
             if 1.5<=ratio<=3.5: # Chon cac contour dam bao ve ratio w/h
-                if h/roi.shape[0]>=0.6: # Chon cac contour cao tu 60% bien so tro len
+                if h/roi.shape[0]>=0.68: # Chon cac contour cao tu 60% bien so tro len
     
                     # Ve khung chu nhat quanh so
                     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
@@ -265,17 +291,17 @@ if (len(LpImg)):
     
                     plate_info +=result
     
-        cv2.imshow("Cac contour tim duoc", img)
-        cv2.waitKey()
-    
-        # Viet bien so len anh
-        cv2.putText(Ivehicle,fine_tune(plate_info),(50, 50), cv2.FONT_HERSHEY_PLAIN, 3.0, (0, 0, 255), lineType=cv2.LINE_AA)
-    
-        # Hien thi anh
-        print("Bien so=", plate_info)
-        cv2.imshow("Hinh anh output",Ivehicle)
-        cv2.waitKey()
-    
+    cv2.imshow("Cac contour tim duoc", img)
+    cv2.waitKey()
+
+    # Viet bien so len anh
+    cv2.putText(Ivehicle,fine_tune(plate_info),(50, 50), cv2.FONT_HERSHEY_PLAIN, 3.0, (0, 0, 255), lineType=cv2.LINE_AA)
+
+    # Hien thi anh
+    print("Bien so=", plate_info)
+    cv2.imshow("Hinh anh output",Ivehicle)
+    cv2.waitKey()
+
 
 
 cv2.destroyAllWindows()
